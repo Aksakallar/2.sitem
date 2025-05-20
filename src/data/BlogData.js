55aaae0d10007ac5098b95100c95efafa52bb637
Mehmetasker <31123405+Aksaka7@users.noreply.github.com>
2025-05-20 12:20:35 +0200

    Update BlogData.js
    Blog tasarımı Tamam

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -1,29 +1,61 @@
 export const Blogs = [
-    {
-        id:1,
-        name:"I'm designing my blogs for now",
-        tags:["react","sass","styled-components"],
-        date:"23 April, 2022",
-        imgSrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ32TQyWJ0OM17k2WKjhzaz9vPyphhtx6V23w&usqp=CAU",
-        link:""
-    }, 
-    {
-        id:1,
-        name:"I'm designing my blogs for now",
-        tags:["react","sass","styled-components"],
-        date:"23 April, 2022",
-        imgSrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ32TQyWJ0OM17k2WKjhzaz9vPyphhtx6V23w&usqp=CAU",
-        link:""
-    },
+   [
+ {
+    "id": 1,
+    "name": "My Work on Blog Design",
+    "tags": ["React", "SASS", "UI Design"],
+    "date": "April 23, 2025",
+    "imgSrc": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ32TQyWJ0OM17k2WKjhzaz9vPyphhtx6V23w&usqp=CAU",
+    "link": ""
+  },
+  {
+    "id": 2,
+    "name": "Developing My Personal Portfolio Page",
+    "tags": ["HTML", "CSS", "JavaScript"],
+    "date": "April 26, 2025",
+    "imgSrc": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ32TQyWJ0OM17k2WKjhzaz9vPyphhtx6V23w&usqp=CAU",
+    "link": ""
+  },
+  {
+    "id": 3,
+    "name": "Minimalist E-Commerce Interface Design",
+    "tags": ["Shopify", "Figma", "Design Thinking"],
+    "date": "April 28, 2025",
+    "imgSrc": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ32TQyWJ0OM17k2WKjhzaz9vPyphhtx6V23w&usqp=CAU",
+    "link": ""
+  },
+  {
+    "id": 4,
+    "name": "Dropshipping Product Listing Automation",
+    "tags": ["JavaScript", "Automation", "E-Commerce"],
+    "date": "May 2, 2025",
+    "imgSrc": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM1p8UhMuFyZbN3cf9qH4YkD_3wBTAqlDbFw&usqp=CAU",
+    "link": ""
+  },
+  {
+    "id": 5,
+    "name": "A Simple and Fast About Page",
+    "tags": ["Next.js", "Tailwind", "Responsive Design"],
+    "date": "May 4, 2025",
+    "imgSrc": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu3yAF7XwhTA3im4vnMdQfG8BNlsOOSVbVSw&usqp=CAU",
+    "link": ""
+  },
+  {
+    "id": 6,
+    "name": "Multi-Language Promotional Site Design",
+    "tags": ["i18n", "React", "UX"],
+    "date": "May 8, 2025",
+    "imgSrc": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkwfE7eMKh-oKve_rjZRD9XD4IfWmDi5q5sw&usqp=CAU",
+    "link": ""
+  },
+  {
+    "id": 7,
+    "name": "Form Site Application",
+    "tags": ["React", "Formik", "TailwindCSS"],
+    "date": "May 10, 2025",
+    "imgSrc": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9nZp2p1Bg9QlweulOCcURe3Tj3lVgzrBuNg&usqp=CAU",
+    "link": ""
+  }
 
-    {
-        id:1,
-        name:"I'm designing my blogs for now",
-        tags:["react","sass","styled-components"],
-        date:"23 April, 2022",
-        imgSrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ32TQyWJ0OM17k2WKjhzaz9vPyphhtx6V23w&usqp=CAU",
-        link:""
-    },
-  
   
 ] 
